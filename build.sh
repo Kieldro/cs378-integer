1bf164a26789c443f6ce524fa81ce3fa47e91dd9
Ian Buitrago <kieldro@gmail.com>
2012-06-22 14:09:20 -0500

    fixed file names in build file.

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 # file variables
-source="RunSMP.c++"
-unitFile="TestSMP.c++"
-inFile="RunSMP.in"
-outFile="RunSMP.out"
+source="RunInteger.c++"
+unitFile="TestInteger.c++"
+inFile="RunInteger.in"
+outFile="RunInteger.out"
 unit=false
 
 if $unit; then
@@ -10,7 +10,7 @@ echo COMPILING UNIT TESTS...
 g++ -ansi -pedantic -lcppunit -ldl -Wall $unitFile -o $unitFile.app
 if ([ $? == 0 ]); then
 echo RUNNING UNIT TESTS...
-./$unitFile.app >& TestSMP.out
+./$unitFile.app #>& TestInteger.out
 #valgrind $unitFile.app >& TestSMP.out
 fi fi
 		
@@ -22,17 +22,18 @@ echo RUNNING PROGRAM...
 ./$source.app < $inFile #>& $outFile
 #valgrind ./$source.app < $inFile >& $outFile
 <<MULTICOMMENT
-echo CHECKING OUTPUT...; diff -lc RunSMP.out RunSMP.in
+echo CHECKING OUTPUT...; diff -lc RunInteger.out RunInteger.in
 
 echo GENERATING COMMIT LOG...
-git log > SMP.log
+git log > Integer.log
 #echo RUNNING DOXYGEN...; doxygen Doxyfile
 
 #echo UPDATING SPHERE FILE...; cp $source Sphere$source
 
-zip SMP README.txt html/* RunSMP.c++ RunSMP.h RunSMP.in RunSMP.out \
-SMP.h SMP.log SphereSMP.c++ TestSMP.c++ TestSMP.out
+zip Integer README.txt html/* RunInteger.c++ RunInteger.h RunInteger.in \
+RunInteger.out Integer.h Integer.log \
+SphereInteger.c++ TestInteger.c++ TestInteger.out
 
-turnin --submit inbleric cs378pj2 SMP.zip
+turnin --submit reza cs378pj3 Integer.zip
 MULTICOMMENT
 fi fi
