71d2651342a82caafe48a0bb965b130d5ef48c41
Ian Buitrago <kieldro@gmail.com>
2012-07-02 12:52:57 -0500

    error in muldigits test 2.

--- build.sh
+++ build.sh
@@ -21,13 +21,14 @@ if ([ $? == 0 ]); then		# if no errors
 echo RUNNING PROGRAM...
 ./$source.app #>& $outFile
 #valgrind ./$source.app < $inFile >& $outFile
+
+#echo CHECKING OUTPUT...; diff -lc RunInteger.out RunInteger.in
 <<MULTICOMMENT
-echo CHECKING OUTPUT...; diff -lc RunInteger.out RunInteger.in
 
 echo GENERATING COMMIT LOG...
 git log > Integer.log
 
-#echo RUNNING DOXYGEN...; doxygen Doxyfile
+echo RUNNING DOXYGEN...; doxygen Doxyfile
 
 zip Integer README.txt html/* RunInteger.c++ RunInteger.h RunInteger.in \
 RunInteger.out Integer.h Integer.log TestInteger.c++ TestInteger.out
