8d24601283c578c5b210726e50adf7b1c2748926
Ian Buitrago <kieldro@gmail.com>
2012-07-02 20:09:45 -0500

    Annihilate Ryans commit.

--- build.sh
+++ build.sh
@@ -10,7 +10,7 @@ echo COMPILING UNIT TESTS...
 g++ -ansi -pedantic -ldl -Wall $unitFile -lcppunit -o $unitFile.app
 if ([ $? == 0 ]); then
 echo RUNNING UNIT TESTS...
-#./$unitFile.app #>& TestInteger.out
+./$unitFile.app #>& TestInteger.out
 #valgrind $unitFile.app >& TestInteger.out
 fi fi
 		
@@ -19,10 +19,11 @@ echo COMPILING PROGRAM...
 g++ -ansi -pedantic -Wall $source -o $source.app
 if ([ $? == 0 ]); then		# if no errors
 echo RUNNING PROGRAM...
-#./$source.app #>& $outFile
+./$source.app #>& $outFile
 #valgrind ./$source.app >& $outFile
 
 #echo CHECKING OUTPUT...; diff -lc RunInteger.out RunInteger.in
+<<MULTICOMMENT
 
 echo GENERATING COMMIT LOG...
 git log > Integer.log
@@ -33,7 +34,7 @@ zip Integer README.txt html/* Integer.h Integer.log RunInteger.c++ \
  RunInteger.out TestInteger.c++ TestInteger.out
 
 turnin --submit reza cs378pj3 Integer.zip
+turnin --list   reza cs378pj3
 turnin --verify reza cs378pj3
-<<MULTICOMMENT
 MULTICOMMENT
 fi fi
